cf46169539ddfb365e32221fb8ebfc9f2d3e7bb7
felixchua3141/2/3 <132286580+felixchua3143@users.noreply.github.com>
2024-06-05 10:30:51 +0800

    s

---
+++ Tutorial/Week 7 + 8/script.js
@@ -0,0 +1,8 @@
+function friends() {
+    var a = prompt("What is your name?");
+    var b = prompt("What's your age");
+    var c = prompt("How many friends do you want to have in a year");
+
+    document.write("Oh " + a + ", by age " + (b + 5) + " , you will have " + (c * 5));
+
+}
