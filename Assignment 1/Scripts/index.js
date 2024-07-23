69c1eb638438892851c1f1a9d74a06743c487de2
felixchua3143 <felixchua3142@gmail.com>
2024-07-23 22:21:15 +0800

    s

--- Assignment 1/Scripts/index.js
+++ Assignment 1/Scripts/index.js
@@ -1,11 +1,9 @@
-function navLoad() {
-    document.getElementById("navbar").innerHTML =
-        '\
+document.getElementById("navbar").innerHTML =
+            '\
             <a href="./index.html"> Home </a> \
             <a href="./showcase.html" > Showcase </a> \
             <a href="./shop.html"> Shop </a> \
             <a href="./checkout.html"> Checkout </a> \
             <a href="./contactus.html"> Contact Us </a> \
-        '
-}
+'
 
