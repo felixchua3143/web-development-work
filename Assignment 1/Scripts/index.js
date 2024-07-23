4b5e536dd0613a6fed7d800d74fa212dc3677441
felixchua3143 <felixchua3142@gmail.com>
2024-07-23 22:15:26 +0800

    s

---
+++ Assignment 1/Scripts/index.js
@@ -0,0 +1,9 @@
+document.getElementById("navbar").innerHTML =
+            '\
+            <a href="./index.html"> Home </a> \
+            <a href="./showcase.html" > Showcase </a> \
+            <a href="./shop.html"> Shop </a> \
+            <a href="./checkout.html"> Checkout </a> \
+            <a href="./contactus.html"> Contact Us </a> \
+'
+
