84c5f309edf1864164496acc96601fc55f8c97fe
felixchua3141/2/3 <132286580+felixchua3143@users.noreply.github.com>
2024-07-25 11:51:49 +0800

    s

--- Assignment 1/Scripts/index.js
+++ Assignment 1/Scripts/index.js
@@ -1,9 +1,10 @@
 document.getElementById("navbar").innerHTML =
             '\
             <a href="./index.html"> Home </a> \
+            <a class="special" href="./aboutus.html"> About Us </a> \
             <a href="./showcase.html" > Showcase </a> \
             <a href="./shop.html"> Shop </a> \
             <a href="./checkout.html"> Checkout </a> \
-            <a href="./contactus.html"> Contact Us </a> \
+            <a class="special" href="./contactus.html"> Contact Us </a> \
 '
 
