3ec26aa7454b21948db683f031d1a1144d2c1cef
felixchua3143 <felixchua3142@gmail.com>
2024-07-28 19:57:29 +0800

    s

---
+++ Assignment 1/Scripts/products.js
@@ -0,0 +1,26 @@
+const products = [
+    {
+        id: 0,
+        name: "Package 1",
+        price: 2000,
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "/Assignment 1/Media/Spring-Wedding-Show.jpg",
+    },
+    {
+        id: 1,
+        name: "Package 2",
+        price: 3000,
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "./img/t2.png",
+    },
+    {
+        id: 2,
+        name: "Package 3",
+        price: 4000,
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "./img/t3.png",
+    },
+];
